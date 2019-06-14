ed465558dd301857647e90c6a31c8ed20b147489
Jorge Arada <jorge.arada@farfetch.com>
2019-06-14 17:32:09 +0100

    UncaughtExceptionHandler method

--- src/main/scala/jorge/arada/LoggingTutorial.scala
+++ src/main/scala/jorge/arada/LoggingTutorial.scala
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory
 object LoggingTutorial
   extends App {
 
+  //setDefaultUncaughtExceptionHandler()
+
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   val a = new Exception("Este erro")
@@ -14,9 +16,9 @@ object LoggingTutorial
 
     """{"Alfaces": {"esta_key":"este value"}}"""
 
-  //logger.info(json,a)
+  logger.info(json,a)
 
-  logger.info("Hello there!",a)
+  //logger.info("Hello there!",a)
 
   /* logger.debug("Hello there!")
    logger.error("Hello there!")
@@ -24,6 +26,25 @@ object LoggingTutorial
    logger.trace("Hello there!")
  */
 
-
   /*logger.info("Este erro", a)*/
+
+/*  def setDefaultUncaughtExceptionHandler(): Unit = {
+
+    try {
+      Thread.setDefaultUncaughtExceptionHandler(
+        (t: Thread, e: Throwable) => {
+          e match {
+            case
+              a: Exception => logger.error("Uncaught Exception detected in thread " + t, a)
+          }
+        }
+      )
+    }
+    catch {
+      case e: SecurityException =>
+        logger.error("Could not set the Default Uncaught Exception Handler", e)
+    }
+  }*/
 }
+
+
