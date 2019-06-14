cbf34a1183640310c3cdc62188f98285eb2e14ab
Jorge Arada <jorge.arada@farfetch.com>
2019-06-14 11:10:47 +0100

    Correct format

--- src/main/scala/jorge/arada/LogDocument.java
+++ src/main/scala/jorge/arada/LogDocument.java
@@ -3,12 +3,12 @@ package jorge.arada;
 import java.io.Serializable;
 import java.util.HashMap;
 
-public class Teste implements Serializable {
+public class LogDocument implements Serializable {
 	public final String Message;
 	public final java.util.HashMap<String, Object> Data;
 
 
-	public Teste(String message, HashMap<String, Object> data) {
+	public LogDocument(String message, HashMap<String, Object> data) {
 		this.Message = message;
 		this.Data = data;
 	}
